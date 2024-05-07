465d8d95e2c90d5046a49e848c751cc9b797ba75
Ahmadsultan395 <ahmadsultan395@gmail.com>
2024-05-07 10:40:58 -0700

    color changing background

--- tailwind/src/App.jsx
+++ tailwind/src/App.jsx
@@ -5,12 +5,23 @@ import './App.css'
 import Card from './assets/components/Card'
 
 function App() {
- 
+
+  const [color, setColor]=useState("olive");
+
 let discription = "I am a software developer fully master in skils and good in commnication skills."
   return (
     <>
     <h1 className='mb-5'>card</h1>
     <Card username="Ahmad Sultan" dis={discription}  btn2="facebook" btn3="tweter" />
+   
+   <div className='mt-4 w-full h-screen' style={{backgroundColor:color}}>
+    <div className='fixed bottom-11'>
+      <button onClick={()=> setColor("red")} className='bg-red-700 ml-3  p-4 rounded-full'>red</button>
+      <button onClick={()=> setColor("green")} className='bg-green-700 ml-3  p-4 rounded-full'>red</button>
+      <button onClick={()=> setColor("black")} className='bg-black-700  ml-3 p-4 rounded-full'>red</button>
+      <button onClick={()=> setColor("pink")} className='bg-pink-700 ml-3  p-4 rounded-full'>red</button>
+    </div>
+   </div>
     </>
   )
 }
